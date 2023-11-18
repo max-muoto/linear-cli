fbbd2a0b4a48ec7a8c448edacb1530b94b0e7fd1
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2023-11-18 14:43:30 -0600

    Setup basic argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
 }
