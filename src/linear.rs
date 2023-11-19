286cc367d40c23d761b292dbdeb09b1281c5256d
Maxwell Muoto <41130755+max-muoto@users.noreply.github.com>
2023-11-18 19:30:00 -0600

    Get teams progress

--- src/linear.rs
+++ src/linear.rs
@@ -1,6 +1,8 @@
 use graphql_client::GraphQLQuery;
 use std::env;
 
+const API_ROOT: &str = "https://api.linear.app/graphql";
+
 struct Team {
     id: String,
     name: String,
@@ -25,39 +27,42 @@ struct Issue {
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "src/schemas/linear_schema.json",
-    query_path = "src//queries/union_query.graphql"
+    query_path = "src/queries/teams.graphql"
 )]
-pub struct UnionQuery;
+pub struct Teams;
 
 struct LinearClient {
     api_key: String,
+    client: reqwest::Client,
 }
 
 impl LinearClient {
     async fn new() -> Self {
         let api_key = env::var("LINEAR_API_KEY").expect("LINEAR_API_KEY not found in .env file");
-        LinearClient { api_key }
+        let client = reqwest::Client::new();
+        LinearClient { api_key, client }
     }
 
-    async fn get_teams(&self) -> Result<Vec<Team>, dyn std::error::Error> {
-        // implement API call to get teams
-        Ok(vec![])
+    /// Makes a request to the Linear API with a given query.
+    async fn make_request(
+        &self,
+        query: graphql_client::QueryBody<Any>,
+    ) -> Result<reqwest::Response, reqwest::Error> {
+        let request = self
+            .client
+            .post(API_ROOT)
+            .header("Authorization", format!("Bearer {}", self.api_key))
+            .json(&query)
+            .send()
+            .await?;
+
+        Ok(request)
     }
 
-    async fn get_users(&self) -> Result<Vec<User>, dyn std::error::Error> {
-        // implement API call to get users
-        Ok(vec![])
+    /// Gets a list of teams from the Linear API.
+    async fn get_teams(&self) {
+        let req_body = Teams::build_query(teams::Variables {});
+        let mut res = self.make_request(req_body).await?;
+        // let response_body: Response<teams::ResponseData> = res.json().await?;
     }
-
-    async fn create_issue(&self /* parameters */) -> Result<Issue, dyn std::error::Error> {
-        // implement API call to create an issue
-        Ok(Issue {
-            id: String::new(),
-            title: String::new(),
-            description: String::new(),
-            // ... initialize other fields
-        })
-    }
-
-    // ... other methods
 }
